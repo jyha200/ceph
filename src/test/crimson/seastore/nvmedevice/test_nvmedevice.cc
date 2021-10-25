6dc19f7ce98273fecdf736764d5adcd60e3932ce
Jinyong Ha <jy200.ha@samsung.com>
2021-10-25 14:23:33 +0900

    seastore : add the use case of write data protection
    1. Add test case that represents simple use case of write data
    protection.
    2. Apply data protection to RandomBlockManager. It omits
    generating CRC if the write protection is enabled
    
    Signed-off-by: Jinyong Ha <jy200.ha@samsung.com>

--- src/test/crimson/seastore/nvmedevice/test_nvmedevice.cc
+++ src/test/crimson/seastore/nvmedevice/test_nvmedevice.cc
@@ -11,6 +11,13 @@ using namespace crimson::os;
 using namespace crimson::os::seastore;
 using namespace nvme_device;
 
+#define TEST_REAL_DEVICE (1)
+#if TEST_REAL_DEVICE
+#define DEV_NAME "/dev/nvme0n1"
+#else
+#define DEV_NAME "ramdomblock_manager.test_nvmedevice"
+#endif
+
 struct nvdev_test_t : seastar_test_suite_t {
   std::unique_ptr<NVMeBlockDevice> device;
   std::string dev_path;
@@ -19,14 +26,18 @@ struct nvdev_test_t : seastar_test_suite_t {
 
   nvdev_test_t() :
     device(nullptr),
-    dev_path("randomblock_manager.test_nvmedevice" + stringify(getpid())) {
+    dev_path(DEV_NAME) {
     int fd = ::open(dev_path.c_str(), O_CREAT|O_RDWR|O_TRUNC, 0644);
     ceph_assert(fd >= 0);
+#if !TEST_REAL_DEVICE
     ::ftruncate(fd, DEV_SIZE);
+#endif
     ::close(fd);
   }
   ~nvdev_test_t() {
+#if !TEST_REAL_DEVICE
     ::unlink(dev_path.c_str());
+#endif
   }
 };
 
@@ -86,3 +97,77 @@ TEST_F(nvdev_test_t, write_and_verify_test)
   });
 }
 
+TEST_F(nvdev_test_t, write_protection_usecase)
+{
+  run_async([this] {
+    device = NVMeBlockDevice::create<PosixNVMeDevice>();
+    device->open(dev_path, seastar::open_flags::rw).unsafe_get();
+    const uint32_t CHECKSUM_OFFSET = BLK_SIZE * 2;
+
+    // An example use case of write data protection
+    // If user wish to protect their data by checksum like CRC32, they should
+    // calculate checksum at the write and verify data via checksum at the read
+    // in the normal case.
+    // However, if the write protection is supported and enabled, user can
+    // offload checksum calculation and verification, and omit checksum relative
+    // steps.
+    bool data_protection_enabled = device->is_data_protection_enabled();
+    nvdev_test_block_t original_data;
+    memset(original_data.data, 0x8, BUF_SIZE);
+    {
+      bufferlist bl;
+      encode(original_data, bl);
+      bl_length = bl.length();
+      auto write_buf = ceph::bufferptr(buffer::create_page_aligned(BLK_SIZE));
+      bl.begin().copy(bl_length, write_buf.c_str());
+
+      // If data protection is disabled, user should calculate and store
+      // checksum.
+      if (data_protection_enabled == false) {
+        uint32_t checksum = ceph_crc32c(0, original_data.data, BLK_SIZE);
+        buffer
+        nvdev_test_block_t checksum_data;
+        ((uint32_t*)(checksum_data.data))[0] = checksum;
+        device->write(BLK_SIZE, write_buf).unsafe_get();
+        bufferlist bl;
+        encode(checksum_data, bl);
+        bl_length = bl.length();
+        auto checksum_buf = ceph::bufferptr(buffer::create_page_aligned(BLK_SIZE));
+        bl.begin().copy(bl_length, checksum_buf.c_str());
+        device->write(CHECKSUM_OFFSET, checksum_buf).unsafe_get();
+      }
+
+      device->write(0, write_buf).unsafe_get();
+    }
+
+    nvdev_test_block_t read_data;
+    {
+      auto read_buf = ceph::bufferptr(buffer::create_page_aligned(BLK_SIZE));
+      device->read(0, read_buf).unsafe_get();
+      bufferlist bl;
+      bl.push_back(read_buf);
+      auto bliter = bl.cbegin();
+      decode(read_data, bliter);
+
+      // If data protection is disabled, user should verify data by reading
+      // previously written checksum and calculate new checksum from data
+      if (data_protection_enabled == false) {
+        nvdev_test_block_t checksum_data;
+        auto checksum_buf = ceph::bufferptr(buffer::create_page_aligned(BLK_SIZE));
+        device->read(CHECKSUM_OFFSET, checksum_buf).unsafe_get();
+        bufferlist bl;
+        bl.push_back(checksum_buf);
+        auto bliter = bl.cbegin();
+        decode(checksum_data, bliter);
+        uint32_t written_checksum = ((uint32_t*)(checksum_data.data))[0];
+        uint32_t data_checksum = ceph_crc32c(0, read_data.data, BLK_SIZE);
+        ASSERT_TRUE(written_checksum == data_checksum);
+      }
+    }
+
+    printf("block size: %ld\n", device->get_block_size());
+    device->close().wait();
+    ASSERT_TRUE(true);
+    device.reset(nullptr);
+  });
+}
