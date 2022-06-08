9bd67748d74dbfeb257c1c3d8e37d0530249f4b7
Jinyong Ha <jy200@gmail.com>
2022-06-08 14:47:17 +0900

    change test code for remote execution

--- test_script/test_01.sh
+++ test_script/test_01.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-targets="_64"
-
-#for i in 32 16 8 4 2 1
-for i in 16 8 4 2 1
-do
-  for target in $targets
-  do
-    sudo timeout -s 9 300 ./test_zns_fs.sh $i $target 2>&1 | tee result_zns_fs_${i}$target
-  done
-done
-
-exit 0
-#targets="vanilla multi_zone postpone_meta append append_multi_qd append_no_lock"
-
+targets="vanilla append_no_lock"
 
 for i in 32 16 8 4 2 1
 do
+  sudo timeout -s 9 400 ./test_zns_fs.sh $i _64 2>&1 | tee result_zns_fs_$i
+  sudo timeout -s 9 400 ./test_cns.sh $i 2>&1 | tee result_cns_$i
   for target in $targets
   do
-    sudo timeout -s 9 300 ./test_zns.sh $i $target 2>&1 | tee result_zns_${target}_$i
+    sudo timeout -s 9 400 ./test_zns.sh $i $target 2>&1 | tee result_zns_${target}_$i
   done
+  sudo timeout -s 9 400 ./test_zns_cns_fs.sh $i 2>&1 | tee result_zns_cns_fs_$i
 done
 
--- test_script/test_zns_fs.sh
+++ test_script/test_zns_fs.sh
@@ -2,15 +2,23 @@
 
 NUM_RBD=$1
 
-sudo ./bringup_zns_fs.sh $2
+execute_remote_cmd() {
+	pswd=$(cat ./pswd)
+	echo "sudo sshpass -f ./pswd ssh $2@$3 \"echo $pswd | sudo -S $1\""
+	sudo sshpass -f ./pswd ssh $2@$3 "echo $pswd | sudo -S $1"
+}
+
+execute_remote_cmd "pkill -9 ceph" jyha 10.0.0.40
+execute_remote_cmd "/home/jyha/mnt/my_ceph/ceph/test_script/bringup_zns_fs.sh $2" jyha 10.0.0.40
+sudo sshpass -f ./pswd scp jyha@10.0.0.40:/etc/ceph/ceph.client.admin.keyring /etc/ceph
+
 sudo ../build/bin/ceph osd pool create bench 32 32
 RBDS=$(seq 1 $NUM_RBD)
-#FIO_CMD="sudo fio --ioengine=rbd --clientname admin --pool bench --ramp_time 1m --bs 4k --readwrite randwrite --invalidate 0 --direct 1 --group_reporting --time_based --runtime 2m --numjobs 1 --thread --iodepth 32"
+sudo ../build/bin/rbd create test_1 -p bench --size 4T --image-shared
 FIO_CMD="sudo fio --ioengine=rbd --clientname admin --pool bench --bs 4k --eta-interval 1 --eta-newline 1 --readwrite randwrite --invalidate 0 --direct 1 --group_reporting --time_based --runtime 3m --numjobs 1 --thread --iodepth 32"
 for i in $RBDS
 do
-  sudo ../build/bin/rbd create test_$i -p bench --size 10G
-  FIO_CMD="$FIO_CMD --name job_$i --rbdname test_$i"
+  FIO_CMD="$FIO_CMD --name job_$i --rbdname test_1"
 done
 echo $FIO_CMD
 $FIO_CMD
